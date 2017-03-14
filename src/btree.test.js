88a59b91021fe4cc9f3f754a9c5e69dafcf84a5d
Duknam Yoo <yoo2001818@gmail.com>
2017-03-14 23:42:53 +0900

    Add MemoryIO test

--- src/btree.test.js
+++ src/btree.test.js
@@ -1,6 +1,7 @@
 import BTree from './btree';
 import { N } from './node';
 import spreadAsyncIterable from './util/spreadAsyncIterable';
+import test from './btree.test.common';
 
 describe('BTree', () => {
   // Use direct I/O without using IDs for ease of debugging
@@ -20,81 +21,7 @@ describe('BTree', () => {
     }, 2, (a, b) => a - b);
     rootNode = null;
   });
-  describe('#insert', () => {
-    it('should return itself', async () => {
-      expect(await btree.insert(1, 1)).toBe(btree);
-    });
-    it('should work on empty root', async () => {
-      expect(await btree.insert(1, 1)).toBe(btree);
-      expect(await spreadAsyncIterable(btree)).toEqual([1]);
-    });
-    it('should sort randomized array to 0..99', async () => {
-      let arr = [];
-      let answer;
-      for (let i = 0; i < 100; ++i) {
-        arr.push(i);
-      }
-      answer = arr.slice();
-      // Use simple shuffle algorithm
-      for (let i = 99; i > 0; --i) {
-        let j = Math.random() * i | 0;
-        let tmp = arr[j];
-        arr[j] = arr[i];
-        arr[i] = tmp;
-      }
-      for (let i = 0; i < 100; ++i) {
-        expect(await btree.insert(arr[i], arr[i])).toBe(btree);
-      }
-      expect(await spreadAsyncIterable(btree)).toEqual(answer);
-    });
-  });
-  describe('#remove', () => {
-    it('should remove node 0 to 9', async () => {
-      for (let i = 0; i < 10; ++i) await btree.insert(i, i);
-      for (let i = 0; i < 9; ++i) expect(await btree.remove(i)).toBe(true);
-      expect(await spreadAsyncIterable(btree)).toEqual([9]);
-    });
-    it('should return false if failed to find node', async () => {
-      for (let i = 0; i < 10; ++i) await btree.insert(i, i);
-      expect(await btree.remove(53)).toBe(false);
-      expect(await btree.remove(-49)).toBe(false);
-    });
-  });
-  describe('#get', () => {
-    beforeEach(async () => {
-      for (let i = 0; i < 100; ++i) {
-        await btree.insert(i, i + 31);
-      }
-    });
-    it('should return right value', async () => {
-      for (let i = 0; i < 100; ++i) {
-        expect(await btree.get(i)).toBe(i + 31);
-      }
-    });
-    it('should return null for invalid values', async () => {
-      expect(await btree.get(50.5)).toBe(null);
-      expect(await btree.get(-33)).toBe(null);
-      // null returns 0, however, since this is comparator function's problem,
-      // that's completely valid.
-      expect(await btree.get(undefined)).toBe(null);
-    });
-  });
-  describe('#smallest', () => {
-    beforeEach(async () => {
-      for (let i = 0; i < 100; ++i) await btree.insert(i, i);
-    });
-    it('should return smallest value', async () => {
-      expect(await btree.smallest()).toBe(0);
-    });
-  });
-  describe('#biggest', () => {
-    beforeEach(async () => {
-      for (let i = 0; i < 100; ++i) await btree.insert(i, i);
-    });
-    it('should return smallest value', async () => {
-      expect(await btree.biggest()).toBe(99);
-    });
-  });
+  test(() => btree);
   describe('#@@asyncIterator', () => {
     beforeEach(() => {
       // Overwrite root node.
---
+++ src/memoryIO.test.js
@@ -0,0 +1,11 @@
+import BTree from './btree';
+import MemoryIO from './memoryIO';
+import test from './btree.test.common';
+
+describe('MemoryIO', () => {
+  let btree;
+  beforeEach(() => {
+    btree = new BTree(new MemoryIO(), 2, (a, b) => a - b);
+  });
+  test(() => btree);
+});
